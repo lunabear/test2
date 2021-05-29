be9e13661b4e7702d6f0cd09414fc0d18d7ddd53
dongom <dongom@DESKTOP-4C0930O>
2021-05-29 19:49:47 +0900

    TEST

---
+++ SpringBatch2/src/main/java/com/dongom/springbatch/dnc/DncMapper.java
@@ -0,0 +1,13 @@
+package com.dongom.springbatch.dnc;
+
+public class DncMapper {
+	private String cusNo;
+
+	public String getCusNo() {
+		return cusNo;
+	}
+
+	public void setCusNo(String cusNo) {
+		this.cusNo = cusNo;
+	}
+}
